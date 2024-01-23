9493e8e49e52002247154464b0de9e17d07d2663
Cesar Miranda <cesarmirandacx602@gmail.com>
2024-01-23 08:51:20 -0700

    saving spot

--- scripts/home.js
+++ scripts/home.js
@@ -9,14 +9,3 @@ const clonedImagesRight = containerRight.querySelector('.sliding-images').cloneN
 // Append the cloned images to their respective containers
 containerLeft.appendChild(clonedImagesLeft);
 containerRight.appendChild(clonedImagesRight);
-
-// Set the animation duration for the cloned images
-clonedImagesLeft.style.animationDuration = '30s'; // Adjust the duration as needed
-clonedImagesRight.style.animationDuration = '35s'; // Adjust the duration as needed
-
-// Start the animation for the original and cloned images for both containers
-containerLeft.querySelector('.sliding-images').style.animation = 'conveyorBelt 30s linear infinite';
-clonedImagesLeft.style.animation = 'conveyorBelt 30s linear infinite';
-
-containerRight.querySelector('.sliding-images').style.animation = 'conveyorBelt 35s linear infinite';
-clonedImagesRight.style.animation = 'conveyorBelt 35s linear infinite';
